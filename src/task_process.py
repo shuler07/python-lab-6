ca7302da845c2e4099617fb77ee837f881ac5acc
shuler7 <vitalisobolevggg@gmail.com>
2026-03-01 22:03:12 +0300

    add TaskProcessor class with process_task(s) methods

--- src/task_process.py
+++ src/task_process.py
@@ -1,13 +1,35 @@
-from typing import List
-
 from src.task import TaskSource
 
 
-def process_tasks(sources: List[TaskSource]) -> None:
-    for source in sources:
-        print("Source:", source.__class__)
-        if not isinstance(source, TaskSource):
-            print(source.__class__, "is wrong tasks source")
+class TaskProcessor:
+    def __init__(self):
+        pass
+
+    def _is_task_source_valid(self, source: TaskSource) -> bool:
+        return isinstance(source, TaskSource)
+
+    def process_task(self, source: TaskSource) -> None:
+        if not self._is_task_source_valid(source):
+            print("Object", source.__class__, "is wrong task source")
+            return
+        print(f"Proccessing task from object {source.__class__}")
+
+        task = source.get_task()
+        if task is not None:
+            print(f"Task #{task.id} processed, payload: {task.payload}")
+        else:
+            print("Task not found")
+
+    def process_tasks(self, source: TaskSource) -> None:
+        if not self._is_task_source_valid(source):
+            print("Object", source.__class__, "is wrong task source")
+            return
+        print(f"Proccessing tasks from object {source.__class__}")
+
         tasks = source.get_tasks()
-        print(f'Got {len(tasks)} tasks')
-        [print(task) for task in tasks]
+        if len(tasks) > 0:
+            print(f"Got {len(tasks)} tasks")
+            for task in tasks:
+                print(f"Task #{task.id} processed, payload: {task.payload}")
+        else:
+            print("Tasks not found")
